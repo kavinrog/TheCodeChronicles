d4570ee561da0556db8f4f2c65ae0f2fddf53ef6
kavinrog <kavinderroghitk@Kavinders-MacBook-Air.local>
2025-02-27 16:02:23 -0800

    Added the dot_product + feedforward + a typo in sigmod->sigmoid

--- SimpleNN/simplenn.py
+++ SimpleNN/simplenn.py
@@ -1,7 +1,7 @@
 import random
 import math
 
-def sigmod(x):
+def sigmoid(x):
   return 1 / 1 +(math.exp(-x))
 
 def sigmoid_derivative(x):
@@ -18,3 +18,11 @@ class SimpleNN:
     
     self.bias_hidden = [random.uniform(-1, 1) for _ in range(hidden_size)]
     self.bias_output = [random.uniform(-1, 1) for _ in range(output_size)]   
+
+def dot_product(self, vector1, vector2):
+    return (x * y for x, y in zip(vector1, vector2))
+
+def feedforward(self, inputs):
+    hidden_layer = [sigmoid(self.dot_product(inputs, [self.weights_input_hidden[j][i] for j in range(self.input_size)]) + self.bias_hidden[i]) for i in range(self.hidden_size)]
+    output_layer = [sigmoid(self.dot_product(hidden_layer, [self.weights_hidden_output[j][i] for j in range(hidden_size)]) + self.bias_ouput[i]) for i in range(self.output_size)]
+    return hidden_layer, output_layer
