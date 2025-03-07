2fe404bf52c502f6e0f4b2f29d65681382be0ebd
kavinrog <kavinderroghit.k@gmail.com>
2025-03-06 19:35:36 -0800

    Add toy dataset, model training + testing and plots

--- LinearLogisticRegression/logisticreg.py
+++ LinearLogisticRegression/logisticreg.py
@@ -18,21 +18,37 @@ class LogisticRegression:
         self.bias = 0
         
         for _ in range(self.iteration):
-            linear_model = np.dot(self.weights, X) + self.bias
+            linear_model = np.dot(X, self.weights) + self.bias
             y_pred = self.sigmoid(linear_model)
             
-            dw = (1/n_samples)*np.dot(X.T, y_pred- y) + self.bias
+            dw = (1/n_samples)*np.dot(X.T, y_pred- y) 
             db = (1/n_samples)*np.sum(y_pred - y)
             
             self.weights-= self.learning_rate * dw
             self.bias-= self.learning_rate * db
             
     def predict(self, X):
-        linear_model = np.dot(self.weights, X) + self.bias
+        linear_model = np.dot(X, self.weights) + self.bias
         y_pred = self.sigmoid(linear_model)
         return [1 if i > 0.5 else 0 for i in y_pred]
             
-            
+np.random.seed(42)
+n_samples = 100
+X = np.random.randn(n_samples, 2)
+y = np.array([1 if x1 + x2 > 0 else 0 for x1, x2 in X])           
                         
-            
-        
+model = LogisticRegression(learning_rate=0.01, iteration=1000)
+model.fit(X, y)
+
+y_pred = model.predict(X)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired, edgecolors='k')
+plt.xlabel('Feature 1')
+plt.ylabel('Feature 2')
+
+xx, yy = np.meshgrid(np.linspace(-3, 3, 100), np.linspace(-3, 3, 100))
+Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
+Z = np.array(Z).reshape(xx.shape)
+plt.contourf(xx, yy, Z, alpha=0.3)
+
+plt.show() 
