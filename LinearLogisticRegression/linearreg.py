ea018a7485cdcd09643fc48008a4c19e01d9ebb6
kavinrog <kavinderroghit.k@gmail.com>
2025-03-06 19:04:26 -0800

    Add the datasets, model testing, plots

--- LinearLogisticRegression/linearreg.py
+++ LinearLogisticRegression/linearreg.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class LinearRegression:
     
@@ -18,13 +19,29 @@ class LinearRegression:
             dw = (1/n_samples) * np.dot(X.T, (y_predicted- y))
             db = (1/n_samples) * np.sum(y_predicted- y)
             
-            self.weights = self.weights + self.learning_rate * dw
-            self.bias = self.bias + self.learning_rate * db
+            self.weights = self.weights - self.learning_rate * dw
+            self.bias = self.bias - self.learning_rate * db
             
     def predict(self, X):
         return np.dot(X, self.weights) + self.bias
-    
-            
-            
+
+np.random.seed(42)
+n_sample = 100
+n_feature =  1    
+X = 2 * np.random.randn(n_sample, n_feature)
+y = (7 + 3 * X + np.random.randn(n_sample, n_feature)).ravel()
+
+model = LinearRegression()
+model.fit(X,y)
+y_pred = model.predict(X)
+print(y_pred)
+
+# Plot the results
+plt.scatter(X, y, color='blue', label='Actual data')
+plt.plot(X, y_pred, color='red', label='Predicted line')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
             
         
