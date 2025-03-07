e1862300b605624988caed11d67690a1aa34b130
kavinrog <kavinderroghit.k@gmail.com>
2025-03-06 18:29:59 -0800

    Add Linear Regression Class with init, fit and predict function

---
+++ LinearLogisticRegression/linearreg.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class LinearRegression:
+    
+    def __init__(self,learning_rate = 0.001, iterations = 1000):
+        self.learning_rate= learning_rate
+        self.iterations = iterations
+        self.weights = None
+        self.bias = None
+        
+    def fit(self, X, y):
+        n_samples , n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias = 0 
+        for i in range(self.iterations):
+            y_predicted = np.dot(X, self.weights)+self.bias
+        
+            dw = (1/n_samples) * np.dot(X.T, (y_predicted- y))
+            db = (1/n_samples) * np.sum(y_predicted- y)
+            
+            self.weights = self.weights + self.learning_rate * dw
+            self.bias = self.bias + self.learning_rate * db
+            
+    def predict(self, X):
+        return np.dot(X, self.weights) + self.bias
+    
+            
+            
+            
+        
