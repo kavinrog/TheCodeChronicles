a15be3a26f7a401d5a8a6d5714430368e2a7f193
kavinrog <kavinderroghit.k@gmail.com>
2025-03-25 19:40:04 -0700

    Add train test split

--- KNN/knn.py
+++ KNN/knn.py
@@ -35,3 +35,5 @@ from sklearn.datasets import load_iris
 iris = load_iris()
 X = iris.data
 y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
